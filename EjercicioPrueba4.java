9c0639d6eaeb5c22c4fde4ad257be101f593208a
jotaeme890 <permanentjavi@gmail.com>
2022-11-22 12:53:13 +0100

    EjercicioPrueba4

---
+++ EjercicioPrueba4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class EjercicioPrueba4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un valor: ");
+        int fila = sc.nextInt();
+        System.out.print("Dime un valor: ");
+        int columna = sc.nextInt();
+        int [] [] matriz = new int [fila] [columna]; //[i] [j]
+
+        for(int i = 0; i<fila ; i++){
+            for(int  j= 0; j<columna;  j++){
+                if(i%2 == 0){
+                    matriz[i][j] = 1;
+                } else{
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 0;i<fila;i++){
+            for(int j= 0;j<columna;j++){
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+        }
+        sc.close();
+    } 
+}
