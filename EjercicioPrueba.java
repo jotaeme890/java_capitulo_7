e6f5fa58c6a9baec4c0a60e10e6f9db65b3505e5
jotaeme890 <permanentjavi@gmail.com>
2022-11-22 12:47:12 +0100

    EjercicioPrueba

--- EjercicioPrueba.java
+++ EjercicioPrueba.java
@@ -1,12 +1,3 @@
-/**
- * 22 Define un array de números enteros de 3 filas por 6 columnas con nombre num
- * y asigna los valores según la siguiente tabla. Muestra el contenido de todos
- * los elementos del array dispuestos en forma de tabla como se muestra en la
- * figura.
- * 
- * @author jotaeme
- */
-
 import java.util.Scanner;
 
 public class EjercicioPrueba {
