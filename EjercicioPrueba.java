032c83d6fbc59360b1efac4890d3400d2f35e4d8
jotaeme890 <permanentjavi@gmail.com>
2022-11-22 13:13:17 +0100

    EjercicioPrueba

--- EjercicioPrueba.java
+++ EjercicioPrueba.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class EjercicioPrueba {
@@ -5,13 +6,11 @@ public class EjercicioPrueba {
         Scanner sc = new Scanner (System.in);
         System.out.print("Dime un valor: ");
         int fila = sc.nextInt();
-        System.out.print("Dime un valor: ");
-        int columna = sc.nextInt();
-        int [] [] matriz = new int [fila] [columna]; //[i] [j]
+        int [] [] matriz = new int [fila] [fila]; //[i] [j]
 
         for(int i = 0; i<fila ; i++){
-            for(int  j= 0; j<columna;  j++){
-                if(i==j || i==0 || j==(columna-1) || j==0 || i==(fila-1) ||i==(fila-j)-1){
+            for(int  j= 0; j<fila;  j++){
+                if(i==j || i==0 || j==(fila-1) || j==0 || i==(fila-1) ||i==(fila-j)-1){
                     matriz[i][j] = 1;
                 } else{
                     matriz[i][j] = 0;
@@ -20,7 +19,7 @@ public class EjercicioPrueba {
         }
 
         for(int i = 0;i<fila;i++){
-            for(int j= 0;j<columna;j++){
+            for(int j= 0;j<fila;j++){
                 System.out.print(matriz[i][j] + " ");
             }
             System.out.println();
