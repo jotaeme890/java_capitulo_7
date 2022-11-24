61e1e393d1dddd1fad019ed62297ff90721c936e
jotaeme890 <permanentjavi@gmail.com>
2022-11-24 09:48:22 +0100

    Ejercicio27

--- Ejercicio27.java
+++ Ejercicio27.java
@@ -15,25 +15,41 @@ public class Ejercicio27 {
         int maximofila= 0;
         int minimocolumna = 0;
         int maximocolumna= 0;
+        boolean repetido;
 
         int [] [] num = new int [6] [10];
 
         for(int i = 0;i<6;i++){
             for(int j = 0; j<10;j++){
-                num [i][j] = (int)(Math.random()*1001);
-                
+                do{
+                    num [i][j] = (int)(Math.random()*1001);
+                    repetido = false;
+
+                    for (int j2 = 0; j2 < 10*i+j; j2++) {
+                        if(num[i][j] == num [j2/10] [j2%10] ){
+                            repetido=true;
+                        }
+                    }
+                }while(repetido);
+            }
+        }
+
+        for(int i = 0;i<6;i++){
+            for(int j = 0; j<10;j++){
+                System.out.printf("%5d ", num[i][j]);
                 if(num[i][j] < minimo){
                     minimo = num[i][j];
-                    minimocolumna = j;
                     minimofila = i;
+                    minimocolumna = j;
                 }
     
                 if(num[i][j] > maximo){
                     maximo = num[i][j];
-                    maximocolumna = j;
                     maximofila = i;
+                    maximocolumna = j;
                 }
             }
+            System.out.println("│");
         }
 
         System.out.println("La posicion mínima es " + minimofila + ", " + minimocolumna);
